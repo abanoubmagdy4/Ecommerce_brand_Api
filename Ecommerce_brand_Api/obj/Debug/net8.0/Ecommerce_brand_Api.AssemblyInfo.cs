70a9af4c4777a5dc784cb6aaeb7cd76432fa8d2e
Abanoub_Magdy <62855040+abanoubmagdy4@users.noreply.github.com>
2025-06-20 23:56:30 +0300

    dd

--- Ecommerce_brand_Api/obj/Debug/net8.0/Ecommerce_brand_Api.AssemblyInfo.cs
+++ Ecommerce_brand_Api/obj/Debug/net8.0/Ecommerce_brand_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce_brand_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d668238b617995082a7f6c13491af2eb0863e1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed7b9a67fed9870baac922775a0b497ee0e4658")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce_brand_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce_brand_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
